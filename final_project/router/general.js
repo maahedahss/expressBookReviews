065ccf69e837c4771bf86a9b0a110d10fa84cf6a
maahedahss <maahedahsadiq@gmail.com>
2025-07-17 12:21:53 -0400

    Updated general methods to use promises and async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,66 +23,83 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Username or password missing. Unable to register user."});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
+// Get the book list available in the shop using async/await and promises
+public_users.get('/',async function (req, res) {
+    let promise = new Promise((resolve, reject) => {
+        resolve(JSON.stringify(books, null, 4));
+    });
+    let result = await promise;
+    res.send(result);
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+// Get book details based on ISBN using async/await and promises
+public_users.get('/isbn/:isbn',async function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    let promise = new Promise((resolve, reject) => {
+        resolve(books[isbn]);
+    });
+    let result = await promise;
+    res.send(result);
 });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+// Get book details based on author using async/await and promises
+public_users.get('/author/:author',async function (req, res) {
     const author = ((req.params.author).toLowerCase()).replaceAll(" ", "");
-    let filteredBooks = {};
 
-    // Get array of keys
-    keys = Object.keys(books);
-    keyInts = [];
-    keys.forEach((key) => {
-        keyInts.push(parseInt(key));
+    let promise = new Promise ((resolve, reject) => {
+        let filteredBooks = {};
+        // Get array of keys
+        keys = Object.keys(books);
+        keyInts = [];
+        keys.forEach((key) => {
+            keyInts.push(parseInt(key));
+        });
+
+        // Check if the author searched for matches any of the authors in books
+        keyInts.forEach((key) => {
+            let currentAuthor = (books[key].author).toLowerCase().replaceAll(" ", "");
+            if (currentAuthor === author) {
+                Object.assign(filteredBooks, {[key]: books[key]});
+            }
+        });
+        resolve(filteredBooks);
     });
 
-    // Check if the author searched for matches any of the authors in books
-    keyInts.forEach((key) => {
-        let currentAuthor = (books[key].author).toLowerCase().replaceAll(" ", "");
-        if (currentAuthor === author) {
-            Object.assign(filteredBooks, {[key]: books[key]});
-        }
-    });
-
-    if (Object.keys(filteredBooks).length > 0) {
-        res.send(filteredBooks);
+    let result = await promise;
+    if (Object.keys(result).length > 0) {
+        res.send(result);
     } else {
         res.send("Author not found");
     }
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+// Get all books based on title using async/await and promises
+public_users.get('/title/:title',async function (req, res) {
     const title = ((req.params.title).toLowerCase()).replaceAll(" ", "");
-    let filteredBooks = {};
 
-    // Get array of keys
-    keys = Object.keys(books);
-    keyInts = [];
-    keys.forEach((key) => {
-        keyInts.push(parseInt(key));
+    let promise = new Promise((resolve, reject) => {
+        let filteredBooks = {};
+
+        // Get array of keys
+        keys = Object.keys(books);
+        keyInts = [];
+        keys.forEach((key) => {
+            keyInts.push(parseInt(key));
+        });
+    
+        // Check if the searched title matches any of the title in books object
+        keyInts.forEach((key) => {
+            let currentTitle = (books[key].title).toLowerCase().replaceAll(" ", "");
+            if (currentTitle === title) {
+                Object.assign(filteredBooks, {[key]: books[key]});
+            }
+        });
+        resolve(filteredBooks);
     });
 
-    // Check if the searched title matches any of the title in books object
-    keyInts.forEach((key) => {
-        let currentTitle = (books[key].title).toLowerCase().replaceAll(" ", "");
-        if (currentTitle === title) {
-            Object.assign(filteredBooks, {[key]: books[key]});
-        }
-    });
-
-    if (Object.keys(filteredBooks).length > 0) {
-        res.send(filteredBooks);
+    let result = await promise;
+    if (Object.keys(result).length > 0) {
+        res.send(result);
     } else {
         res.send("Title not found.");
     }
